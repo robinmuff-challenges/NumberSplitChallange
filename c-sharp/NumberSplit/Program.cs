e13178f96dbb73b54ec7c590466dc4cced7ad4ef
Robin Muff <robin.muff@bluewin.ch>
2022-01-27 10:27:54 +0100

    solved in c#

---
+++ c-sharp/NumberSplit/Program.cs
@@ -0,0 +1,10 @@
+﻿const string inputfile = "C:/users/robin/downloads/input.txt";
+const string outputfile = "C:/users/robin/downloads/output.txt";
+File.Delete(outputfile);
+
+string item = "";
+foreach (Char c in File.ReadAllText(inputfile).Replace(" ", ""))
+{
+    item += c;
+    if (Convert.ToInt32(item) > 120) { File.AppendAllText(outputfile, item.Remove(item.Length - 1, 1) + " "); item = c.ToString(); }
+}
