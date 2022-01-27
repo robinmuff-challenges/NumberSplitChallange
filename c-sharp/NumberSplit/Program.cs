d50cc7006a7d41a9b31f2013326b4faeae9140f1
Robin Muff <robin.muff@bluewin.ch>
2022-01-27 10:53:57 +0100

    Update to none file based

--- c-sharp/NumberSplit/Program.cs
+++ c-sharp/NumberSplit/Program.cs
@@ -1,10 +1,8 @@
-﻿const string inputfile = "C:/users/robin/downloads/input.txt";
-const string outputfile = "C:/users/robin/downloads/output.txt";
-File.Delete(outputfile);
+﻿string content = "1 1 5 9 8 1 3 5 8 1 1 8 7 5 6 1 2 9  1 2 5 8 0 2";
 
 string item = "";
-foreach (Char c in File.ReadAllText(inputfile).Replace(" ", ""))
+foreach (Char c in content.Replace(" ", ""))
 {
     item += c;
-    if (Convert.ToInt32(item) > 120) { File.AppendAllText(outputfile, item.Remove(item.Length - 1, 1) + " "); item = c.ToString(); }
+    if (Convert.ToInt32(item) > 120) { Console.Write(item.Remove(item.Length - 1, 1) + " "); item = c.ToString(); }
 }
